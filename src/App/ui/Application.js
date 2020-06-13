2e3e8153fee2c6329b51fbcf36ef4163de4454ba
techmsi <sophia.idro@gmail.com>
2020-06-13 15:02:34 -0400

    extract Application component

---
+++ src/App/ui/Application.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import throttle from 'lodash/throttle';
+
+import configureStore from 'store/';
+import { loadState, saveState } from 'store/persistState';
+import App from 'App/ui/App';
+
+const persistedState = loadState();
+export const store = configureStore(persistedState);
+store.subscribe(
+  throttle(() => {
+    const { therapists, questionnaire } = store.getState();
+    saveState({
+      therapists,
+      questionnaire
+    });
+  }, 1000)
+);
+
+export const Application = () => {
+  console.log('store ', store.getState());
+  return (
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,39 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 
-import { Provider } from 'react-redux';
-import configureStore from 'store/';
-import { loadState, saveState } from 'store/persistState';
-import throttle from 'lodash/throttle';
-
-import App from 'App/ui/App';
+import { Application } from './App/ui/Application';
 
 import 'styles/reset.css';
 import 'styles/styles.css';
 
-const persistedState = loadState();
-const store = configureStore(persistedState);
-store.subscribe(
-  throttle(() => {
-    const { therapists, questionnaire } = store.getState();
-    saveState({
-      therapists,
-      questionnaire
-    });
-  }, 1000)
-);
-
-const Application = () => {
-  console.log('store ', store.getState());
-
-  return (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-};
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<Application />, rootElement);
-
-export default Application;
+render(<Application />, rootElement);
