96c94364e5230d92a6357041ffcad3b64717c662
techmsi <sophia.idro@gmail.com>
2020-06-14 17:29:04 -0400

    replace with preact

---
+++ config-overrides.js
@@ -0,0 +1,26 @@
+const {
+  override,
+  addWebpackAlias,
+  addBundleVisualizer,
+  addWebpackPlugin,
+} = require('customize-cra');
+const WebpackBar = require('webpackbar');
+const ChunkManifestPlugin = require('chunk-manifest-webpack-plugin');
+const WebpackChunkHash = require('webpack-chunk-hash');
+const analyzer = process.env.NODE_ENV !== 'production' ? addBundleVisualizer(): null;
+module.exports = override(
+  addWebpackAlias({
+    react: 'preact/compat',
+    'react-dom': 'preact/compat',
+  }),
+  addWebpackPlugin(
+    new WebpackBar(),
+    new WebpackChunkHash(),
+    new ChunkManifestPlugin({
+      filename: 'chunk-manifest.json',
+      manifestVariable: 'webpackManifest',
+      inlineManifest: true,
+    })
+  ),
+  analyzer
+);
