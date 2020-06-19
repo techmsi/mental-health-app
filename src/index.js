b6e948555cd910d9c7db138e80ac2c08058b904a
techmsi <sophia.idro@gmail.com>
2020-06-19 12:58:26 -0400

    correct path for index

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import 'preact/debug';
 import React from 'react';
 import { render, hydrate } from 'react-dom';
 
-import * as serviceWorker from './serviceWorker';
-import { Application } from './App/ui/Application';
+import * as serviceWorker from 'worker/serviceWorker';
+import { Application } from 'App/ui/Application';
 
 import 'styles/reset.css';
 
--- src/worker/serviceWorker.js
+++ src/worker/serviceWorker.js
@@ -13,9 +13,6 @@ export function register (config) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
-      // Our service worker won't work if PUBLIC_URL is on a different origin
-      // from what our page is served on. This might happen if a CDN is used to
-      // serve assets; see https://github.com/facebook/create-react-app/issues/2374
       return;
     }
 
@@ -54,9 +51,6 @@ function registerValidSW (swUrl, config) {
         installingWorker.onstatechange = () => {
           if (installingWorker.state === 'installed') {
             if (navigator.serviceWorker.controller) {
-              // At this point, the updated precached content has been fetched,
-              // but the previous service worker will still serve the older
-              // content until all client tabs are closed.
               console.debug(
                 'New content is available and will be used when all ' +
                   'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
@@ -67,9 +61,6 @@ function registerValidSW (swUrl, config) {
                 config.onUpdate(registration);
               }
             } else {
-              // At this point, everything has been precached.
-              // It's the perfect time to display a
-              // "Content is cached for offline use." message.
               console.debug('Content is cached for offline use.');
 
               // Execute callback
