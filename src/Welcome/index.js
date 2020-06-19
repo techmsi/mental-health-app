c8e9ef823b32ef149d3d8b6d14772bc3c30dcb2c
techmsi <sophia.idro@gmail.com>
2020-06-19 19:42:42 -0400

    separtate welcome styles

--- src/Welcome/index.js
+++ src/Welcome/index.js
@@ -2,15 +2,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { Header, DarkButton } from 'styles/Layout';
-import { WelcomeShell } from 'Welcome/ui/styles-Welcome';
+import { Header } from 'styles/Layout';
+import {
+  WelcomeShell,
+  WelcomeText,
+  WelcomeButton
+} from 'Welcome/ui/styles-Welcome';
 
 const Welcome = props => (
   <WelcomeShell className='introduction'>
     <Header>
       <h1>Paitent Health Questionnaire (PHQ-9)</h1>
     </Header>
-    <p className='text'>
+    <WelcomeText>
       This easy to use patient questionnaire is a self-administered version of
       the PRIME-MD diagnostic instrument for common mental disorders. The PHQ-9
       is the depression module, which scores each of the nine DSM-IV criteria as
@@ -19,10 +23,10 @@ const Welcome = props => (
       to monitor the severity of depression and response to treatment. However,
       it can be used to make a tentative diagnosis of depression in at-risk
       populations - eg, those with coronary heart disease or after stroke.
-    </p>
-    <DarkButton className='begin'>
+    </WelcomeText>
+    <WelcomeButton>
       <Link to='/questionnaire'> Begin Questionnaire</Link>
-    </DarkButton>
+    </WelcomeButton>
   </WelcomeShell>
 );
 
--- src/Welcome/ui/styles-Welcome.js
+++ src/Welcome/ui/styles-Welcome.js
@@ -1,26 +1,27 @@
 import styled from 'styled-components';
 import { media } from 'styles/Responsive';
+import { DarkButton } from 'styles/Layout';
+
+export const WelcomeText = styled.p`
+  padding: 1.5rem;
+  ${media.phone`
+     order: 2;
+  `}
+`;
+
+export const WelcomeButton = styled(DarkButton)`
+  ${media.phone`
+     align-self: center;
+     order: 1;
+     max-width: 20rem;
+     margin-top: 1.5rem;
+    a{border-radius: 0.25rem;}
+  `}
+`;
 
 export const WelcomeShell = styled.section`
   line-height: 2rem;
   display: flex;
   flex-direction: column;
   text-align: center;
-
-  .begin {
-    ${media.phone`
-     align-self: center;
-     order: 1;
-     max-width: 20rem;
-     margin-top: 1.5rem;
-    a{border-radius: 0.25rem;}
-    `}
-  }
-
-  .text {
-    padding: 1.5rem;
-    ${media.phone`
-     order: 2;
-    `}
-  }
 `;
