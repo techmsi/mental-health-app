7546354c6b68e28b4c478895e18028314fc47139
techmsi <sophia.idro@gmail.com>
2020-07-15 21:47:12 -0400

    add square images

---
+++ src/crop.js
@@ -0,0 +1,29 @@
+const sharp = require('sharp');
+const { resolve, join, basename } = require('path');
+const { readdirSync } = require('fs');
+const headshotsFolder = resolve('.', 'public/images/headshots/');
+
+const square = () => {
+  try {
+    const headshots = readdirSync(headshotsFolder).filter(file =>
+      file.includes('.jpg')
+    );
+
+    headshots.forEach(imageFile => {
+        const imageFileName = `${imageFile.replace('.jpg', '')}_200.jpg`;
+        const fullImagePath = join(headshotsFolder, imageFile);
+        const fullImageFileName = join(headshotsFolder, imageFileName);
+
+        sharp(fullImagePath)
+          .resize(200, 200)
+          .toFile(fullImageFileName, (err, info) => {
+            if (err) return;
+            console.log(`${imageFileName}`, info.size);
+          });
+    });
+  } catch (error) {
+    console.log('Cropping error', error);
+  }
+};
+
+module.exports = square();
