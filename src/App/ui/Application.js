86ff283e26bad2532d78ca080dcc46c8114a4132
techmsi <sophia.idro@gmail.com>
2020-07-17 22:42:53 -0400

    remove answered from persisted state

--- src/App/ui/Application.js
+++ src/App/ui/Application.js
@@ -4,18 +4,36 @@ import throttle from 'lodash/throttle';
 
 import configureStore from 'store/';
 import { loadState, saveState } from 'store/persistState';
+
 import App from 'App/ui/App';
 import { AppWithBoundary } from 'App/ui/AppWithBoundary';
+import { questionnaire } from 'Questions/reducer-Questions';
 
 const persistedState = loadState();
 const store = configureStore(persistedState);
 store.subscribe(
   throttle(() => {
     const { therapists, questionnaire } = store.getState();
-    saveState({
-      therapists,
-      questionnaire
-    });
+    let questions = questionnaire.questions;
+
+    if (questionnaire.questions) {
+      questions = questionnaire.questions.map(({ answered, ...question }) => ({
+        ...question
+      }));
+
+      saveState({
+        therapists,
+        questionnaire: {
+          ...questionnaire,
+          ...{ questions }
+        }
+      });
+    } else {
+      saveState({
+        therapists,
+        questionnaire
+      });
+    }
   }, 1000)
 );
 
